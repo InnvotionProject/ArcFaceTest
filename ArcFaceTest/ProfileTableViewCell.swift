d4158be141485fdcd2c800e82fb1c920b1b805ec
LeBW <27091925@qq.com>
2018-02-20 01:07:51 +0800

    添加 我的数据 相关UI

---
+++ ArcFaceTest/ProfileTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileTableViewCell.swift
+//  ArcFaceTest
+//
+//  Created by 李博文 on 20/02/2018.
+//  Copyright © 2018 王宇鑫. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
