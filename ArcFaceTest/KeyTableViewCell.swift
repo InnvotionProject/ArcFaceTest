07373921565cccd2c066623f987da97567c2a7bc
jimmy233 <m18684110426@163.com>
2018-03-03 14:21:20 +0800

    MeMeMeMeMeMeMeMeMeMeMeMe

---
+++ ArcFaceTest/KeyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KeyTableViewCell.swift
+//  ArcFaceTest
+//
+//  Created by jimmy233 on 2018/3/3.
+//  Copyright © 2018年 王宇鑫. All rights reserved.
+//
+
+import UIKit
+
+class KeyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
