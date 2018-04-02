27ec06c956adb959556399b4ff0aecf384a17acb
LeBW <27091925@qq.com>
2018-04-02 19:00:07 +0800

    "add a "java" attendance in file "Appdelegate.swift". 该语句已注释。只需运行一次。

--- ArcFaceTest/AppDelegate.swift
+++ ArcFaceTest/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //guard Information.shared.add(attendance: "Java", detail: "Java Class by CaoChun", startTime: Date(timeIntervalSinceNow: 0)) else {
+        //    print("save error")
+        //    return false
+        //}
         return true
     }
 
