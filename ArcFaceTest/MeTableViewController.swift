679bb3340ba220424117611a3d43aba4e2955d66
jimmy233 <m18684110426@163.com>
2018-04-04 12:37:36 +0800

    TableView Modify

--- ArcFaceTest/MeTableViewController.swift
+++ ArcFaceTest/MeTableViewController.swift
@@ -34,7 +34,7 @@ class MeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 6
+        return 7
     }
 
     /*
